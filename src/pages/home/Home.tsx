15a88e803cc2053b4fd9d1ed81f1894d3f867959
Benedictus Yogatama <yogatama4561@gmail.com>
2024-11-23 22:21:19 +0700

    home responsiveness fix2

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./Home.css";
-import { delay, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const words = [
