2cb1e354796a998c2759deaa508a9f42007b445c
Benedictus Yogatama <yogatama4561@gmail.com>
2024-11-23 10:35:02 +0700

    deploy prep 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <NavBar />
