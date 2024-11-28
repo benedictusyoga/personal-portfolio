69892de69df24144f938eb7b06785bbb89078074
Benedictus Yogatama <yogatama4561@gmail.com>
2024-11-28 13:58:38 +0700

    vercel prep

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/personal-portfolio">
+    <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
